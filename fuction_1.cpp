a16fe27bdb71c9a49d1eb7e3538a97209a1b279f
rahatzulfiqar <110724883+rahatzulfiqar@users.noreply.github.com>
2022-08-14 14:38:27 +0500

    Create fuction_1.cpp

---
+++ fuction_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void Line();
+int main()
+{
+	Line();
+	cout<<"Hello"<<endl;
+	Line();
+	cout<<"We are studying programming"<<endl;
+	Line();
+}
+void Line()
+{
+    for(a=1; a<=20; a++)
+    cout<<"*";
+    cout<<endl;
+}
